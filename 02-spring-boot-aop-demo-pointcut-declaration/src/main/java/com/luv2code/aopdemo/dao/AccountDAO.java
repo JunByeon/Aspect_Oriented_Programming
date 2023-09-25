31840e93394852539c5c8dcf896e896f68634bd0
JunByeon <143871011+JunByeon@users.noreply.github.com>
2023-09-25 23:35:31 +0900

    Add files via upload

---
+++ 02-spring-boot-aop-demo-pointcut-declaration/src/main/java/com/luv2code/aopdemo/dao/AccountDAO.java
@@ -0,0 +1,19 @@
+package com.luv2code.aopdemo.dao;
+
+import com.luv2code.aopdemo.Account;
+
+import java.util.List;
+
+public interface AccountDAO {
+    void addAccount(Account theAccount, boolean vipFlag);
+
+    boolean doWork();
+
+    public String getName();
+    public void setName(String name);
+    public String getServiceCode();
+    public void setServiceCode(String serviceCode);
+    public List<Account> findAccounts();
+
+    public List<Account> findAccounts(boolean tripWire);
+}
---
+++ 02-spring-boot-aop-demo-pointcut-declaration/src/main/java/com/luv2code/aopdemo/service/TrafficFortuneService.java
@@ -0,0 +1,8 @@
+package com.luv2code.aopdemo.service;
+
+public interface TrafficFortuneService {
+
+    String getFourtune();
+
+    String getFourtune(boolean tripWire);
+}
